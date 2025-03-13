3d864a6e5dc2a24352573ce6b186370bccf3d2c0
Andualem <andualem.fereja12@gmail.com>
2025-03-14 01:02:32 +0300

    portfolio app created

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site:'https://Andu-alem.github.io',
+  output: 'static',
+  integrations: [react(), mdx(), sitemap()],
+  prefetch: {
+    prefetchAll: true
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Andualem Fereja | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'A skilled software developer with a strong expertise in full-stack development. Whether you need an e-commerce website, a blog to share your insights, a landing page for your business, or a portfolio site to showcase your work, I deliver SEO friendly, high-quality products tailored to your needs';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
